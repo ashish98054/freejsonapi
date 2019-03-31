a251131df2cf199e59d3a5b660c8c4ea92487971
Ashish K. Poudel <ashishp@khozinfo.com>
2019-03-31 23:27:52 +0545

    update createupdatepostcomment request

--- app/Domain/Posts/Requests/CreateUpdatePostCommentRequest.php
+++ app/Domain/Posts/Requests/CreateUpdatePostCommentRequest.php
@@ -9,7 +9,7 @@ final class CreateUpdatePostCommentRequest extends BaseFormRequest
     public function rules()
     {
         return [
-            'body' => 'required|min:5'
+            'body' => 'required'
         ];
     }
 }
